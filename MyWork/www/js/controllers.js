fb89ca6225a1fb332a371a4c3412878703ee5865
wangnan <1085463652@qq.com>
2015-12-24 14:56:05 +0800

    12.24 14:55 company

--- MyWork/www/js/controllers.js
+++ MyWork/www/js/controllers.js
@@ -1,73 +1,223 @@
 angular.module('starter.controllers', [])
 
-.controller('LoanAccountCtrl', function($scope) {})
-.controller('InverstmentAccountCtrl', function($scope) {})
-.controller('TransactionRecordCtrl', function($scope) {
-	$scope.datas=[{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	},{
-		periods:'6/12',
-		principal:1000,
-		interest:120,
-		date:'2015-12-30',
-		time:'18:23:24',
-		status:'已还款'
-	}]
-})
-.controller('AccountCtrl', ['$scope','$state',function($scope,$state) {
-		$scope.GoBasicInfoPage=GoBasicInfoPage;
-		$scope.GoIdentifacationPage=GoIdentifacationPage;
-		function GoBasicInfoPage(){
+.controller('LoanAccountCtrl', function($scope) {
+		$scope.labelschart = ["complete", "uncomplete"];
+		$scope.datachart = [25, 75];
+		$scope.lists=[];
+		var str=[{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		},{
+			index:0,
+			title:'地铁**项目',
+			money:1000,
+			interest:120,
+			date:2015-12-30
+		}]
+		AssmbleArr();
+		function AssmbleArr(){
+			var count=9;
+			var length=str.length;
+			
+			var page=Math.floor((length-1)/count)+1;
+			var list=[];
+			for(var i=0;i<page;i++){
+				var arr=[];
+				var startindex=count*i;
+				var endindex=0;
+				if(startindex+count<length){
+					endindex=startindex+count;
+				}else{
+					endindex=length;
+				}
+				for(var j=startindex;j<endindex;j++){
+					arr.push(str[j])
+				}
+				list.push(arr);
+			}
+			$scope.lists=list;
+		}
+	})
+	.controller('InverstmentAccountCtrl', function($scope) {
+		$scope.labelschart = ["complete", "","","uncomplete"];
+		$scope.datachart = [25,0,0,75];
+		$scope.onClick = function(points, evt) {
+			console.log(points, evt);
+		};
+	})
+	.controller('TransactionRecordCtrl', function($scope) {
+		$scope.datas = [{
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}, {
+			periods: '6/12',
+			principal: 1000,
+			interest: 120,
+			date: '2015-12-30',
+			time: '18:23:24',
+			status: '已还款'
+		}]
+	})
+	.controller('AccountCtrl', ['$scope', '$state', function($scope, $state) {
+		$scope.GoBasicInfoPage = GoBasicInfoPage;
+		$scope.GoIdentifacationPage = GoIdentifacationPage;
+
+		function GoBasicInfoPage() {
 			$state.go('basicInfo');
 		}
-		function GoIdentifacationPage(){
+
+		function GoIdentifacationPage() {
 			$state.go('identification');
 		}
-}]);
+	}]);
